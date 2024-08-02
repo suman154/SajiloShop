1920274af6cec42b40e86d50c35a3e6b1296469d
suman154 <sumanbhatta735@gmail.com>
2024-08-02 07:35:27 +0545

    Shopping Cart Setup

---
+++ cart/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', cart_summary, name='cart_summary'),
+    path('add/', cart_add, name='cart_add'),
+    path('update/', cart_update, name='cart_update'),
+    path('delete/', cart_delete, name='cart_delete'),
+]
--- ecom/settings.py
+++ ecom/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # App
-    'store'
+    'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
