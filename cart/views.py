af3e334129bd43bebe57ffd249963592ba765ba5
suman154 <sumanbhatta735@gmail.com>
2024-08-07 21:09:13 +0545

    Remove Item from cart

--- cart/views.py
+++ cart/views.py
@@ -50,4 +50,12 @@ def cart_update(request):
 
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        # Call the delete function
+        cart.delete(product=product_id)
+
+        response = JsonResponse({'product':product_id})
+        return response
+
